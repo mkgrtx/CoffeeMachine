a3e26df6744ac739c91edace66ad913a2635d10e
mkgrtx <mkgrtx@gmail.com>
2025-05-15 21:42:13 +0100

    Finish stage 4/6

--- main.go
+++ main.go
@@ -4,60 +4,113 @@ import (
 	"fmt"
 )
 
-const (
-	needWater = 200
-	needMilk  = 50
-	needBean  = 15
+var (
+	// water, milk, bean
+	espresso = []int{250, 0, 16, 4}
+	latte    = []int{350, 75, 20, 7}
+	cappu    = []int{200, 100, 12, 6}
+)
+
+var (
+	dWater int = 400
+	dMilk  int = 540
+	dBean  int = 120
+	dCup   int = 9
+	dmoney int = 550
 )
 
 func main() {
-	//	fmt.Println(`Starting to make a coffee
-	//Grinding coffee beans
-	//Boiling water
-	//Mixing boiled water with crushed coffee beans
-	//Pouring coffee into the cup
-	//Pouring some milk into the cup
-	//Coffee is ready!`)
+	status(dWater, dMilk, dBean, dCup, dmoney)
 
-	//amountWater := numCups * 200
-	//amountMilk := numCups * 50
-	//amountBean := numCups * 15
-
-	//fmt.Printf("For %d cups of coffee you will need\n"+
-	//	"%d ml of water\n"+
-	//	"%d ml of milk\n"+
-	//	"%d g of coffee beans\n", numCups, amountWater, amountMilk, amountBean)
-
-	var (
-		dWater int
-		dMilk  int
-		dBean  int
-	)
-
-	fmt.Println("Write how many ml of water the coffee machine has:")
-	fmt.Scanln(&dWater)
-	fmt.Println("Write how many ml of milk the coffee machine has:")
-	fmt.Scanln(&dMilk)
-	fmt.Println("Write how many grams of coffee beans the coffee machine has:")
-	fmt.Scanln(&dBean)
-
-	canMakeWater := dWater / needWater
-	canMakeMilk := dMilk / needMilk
-	canMakeBean := dBean / needBean
-
-	numCanHave := min(canMakeWater, canMakeMilk, canMakeBean)
-
-	fmt.Println("Write how many cups of coffee you will need:")
-	var numCups int
-	fmt.Scanln(&numCups)
-
-	difCanNeed := numCanHave - numCups
-	switch {
-	case difCanNeed == 0:
-		fmt.Println("Yes, I can make that amount of coffee")
-	case difCanNeed < 0:
-		fmt.Printf("No, I can make only %d cups of coffee\n", numCanHave)
-	case difCanNeed > 0:
-		fmt.Printf("Yes, I can make that amount of coffee (and even %d more than that)\n", difCanNeed)
+	fmt.Println("Write action (buy, fill, take): ")
+	var action string
+	fmt.Scanln(&action)
+	switch action {
+	case "buy":
+		buy()
+	case "fill":
+		fill()
+	case "take":
+		take()
 	}
+
+}
+
+func buy() {
+	fmt.Println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+	var choice string
+	fmt.Scanln(&choice)
+
+	switch choice {
+	case "1":
+		makeEspresso()
+	case "2":
+		makeLatte()
+	case "3":
+		makeCappu()
+	}
+}
+
+func makeCappu() {
+	dWater -= cappu[0]
+	dMilk -= cappu[1]
+	dBean -= cappu[2]
+	dmoney += cappu[3]
+	dCup--
+	status(dWater, dMilk, dBean, dCup, dmoney)
+}
+
+func makeLatte() {
+	dWater -= latte[0]
+	dMilk -= latte[1]
+	dBean -= latte[2]
+	dmoney += latte[3]
+	dCup--
+	status(dWater, dMilk, dBean, dCup, dmoney)
+}
+
+func makeEspresso() {
+	dWater -= espresso[0]
+	dMilk -= espresso[1]
+	dBean -= espresso[2]
+	dmoney += espresso[3]
+	dCup--
+	status(dWater, dMilk, dBean, dCup, dmoney)
+}
+
+func fill() {
+	var water, milk, bean, cup int
+	fmt.Println("Write how many ml of water you want to add: ")
+	fmt.Scanln(&water)
+	fmt.Println("Write how many ml of milk you want to add: ")
+	fmt.Scanln(&milk)
+	fmt.Println("Write how many grams of coffee beans you want to add: ")
+	fmt.Scanln(&bean)
+	fmt.Println("Write how many disposable cups you want to add: ")
+	fmt.Scanln(&cup)
+
+	dWater += water
+	dMilk += milk
+	dBean += bean
+	dCup += cup
+
+	status(dWater, dMilk, dBean, dCup, dmoney)
+}
+
+func take() {
+	fmt.Printf("I gave you $%d\n", dmoney)
+	dmoney = 0
+	status(dWater, dMilk, dBean, dCup, dmoney)
+}
+
+func status(dWater int, dMilk int, dBean int, dCup int, dmoney int) {
+
+	fmt.Printf(`The coffee machine has:
+%d ml of water
+%d ml of milk
+%d g of coffee beans
+%d disposable cups
+$%d of money
+`, dWater, dMilk, dBean, dCup, dmoney)
+
 }
